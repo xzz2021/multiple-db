91312349ba2a9d350aa162e20b75ab62d9bf3cd0
xzz2021 <zipeng.2016@gmail.com>
2024-10-31 09:27:55 +0800

    添加seed文件

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+  // create two dummy articles
+  const post1 = await prisma.test666.create({
+    data: {
+      title: 'Prisma Adds Support for MongoDB',
+      authorId: 1,
+      published: false,
+    },
+  });
+
+  console.log({ post1 });
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
